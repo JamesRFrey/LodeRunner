c49f7c7e8dc157ea66f27363ee930161eae7ce08
James R. Frey <JamesRFrey@users.noreply.github.com>
2019-01-08 22:10:04 -0600

    gdx setup

---
+++ desktop/src/com/loderunner/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.loderunner.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.loderunner.game.LodeRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LodeRunner(), config);
+	}
+}
